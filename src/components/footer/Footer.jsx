a802064ebcec4a3a418decb1ab967951cc2481ba
xasher <abdurrahimgayretli@gmail.com>
2023-06-25 02:52:08 +0300

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div>•2023 Abdurrahim. All rights reserved.</div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Footer;
